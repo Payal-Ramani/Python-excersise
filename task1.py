cbca2866245a1fab469f5cf0ba78f7a8d8b4af33
Payal Ramani <payalr.tagline@gmail.com>
2022-12-14 17:51:29 +0530

    modify task1 file

--- task1.py
+++ task1.py
@@ -7,32 +7,29 @@ for i in range(3):
 
 def Display_sum_of_odd_numbers(list):
     sum = 0
-    for i in list:
-        if i % 2 != 0:
-            sum += i
+    for n in list:
+        if n % 2 != 0:
+            sum += n
     return sum
 
 
-empty_list = []
-for i in list_of_numbers:
-        if list_of_numbers.count(i) > 1:
-            empty_list.append(i)
-            newlist = list(set(empty_list))
+empty_list_forCountDuplicates = []
+for num in list_of_numbers:
+    if list_of_numbers.count(num) > 1:
+        if num not in empty_list_forCountDuplicates:
+            empty_list_forCountDuplicates.append(num)
 
-list_without_duplicates = []
-for i in list_of_numbers:
-    if i not in list_without_duplicates:
-        list_without_duplicates.append(i)
+list_without_duplicates = set(list_of_numbers)
 
 input_from_user = input(" A : length of list \n B : Displays first 3 numbers \n C : Displays sum of odd numbers \n D : Number of Duplicate numbers \n E : Display list without duplicate numbers \n Enter Your choice from A,B,C,D,E: " )
 
 if(input_from_user == "A"):
     print(length_of_list)
 if(input_from_user == "B"):
-    print(first_three_numbers)
+    print(list_of_numbers[:3])
 if(input_from_user == "C"):
     print(Display_sum_of_odd_numbers(list_of_numbers))
 if(input_from_user == "D"):
-    print(len(newlist))
+    print(len(empty_list_forCountDuplicates))
 if(input_from_user == "E"):
-    print(list_without_duplicates)
+    print(list(list_without_duplicates))
