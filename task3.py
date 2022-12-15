f13d2f43505b6e487e2b226df6afd5305a3bc850
Payal Ramani <payalr.tagline@gmail.com>
2022-12-15 15:33:26 +0530

    modified task3 file (list name changed)

--- task3.py
+++ task3.py
@@ -2,15 +2,15 @@ from collections import Counter
 sentences = ['My name is Ram', 'He is a good person', 'You should be careful while coding', 'He can do better', 'The person is mysterious', 'Jay Shree Ram', 'It is my pen.']
 
 # Here we did spliting of sentences from the list
-splited_list = []
+splited_sentence = []
 for sentence in sentences:
-    splited_list.append(sentence.split())
-print(splited_list)
+    splited_sentence.append(sentence.split())
+print(splited_sentence)
 
 # here we put all the string in one list
 temp = []
-for list in splited_list:
-    for string in list :
+for word in splited_sentence:
+    for string in word :
         temp.append(string)
 print(temp)
 
