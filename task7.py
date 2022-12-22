aec73b74e595795752c170faa9d114fe56c51439
Payal Ramani <payalr.tagline@gmail.com>
2022-12-22 12:21:19 +0530

    task7 file modified with reducing code

--- task7.py
+++ task7.py
@@ -4,8 +4,5 @@ emails = ['abc@gmail.com', '123$tt*@xyz.com', 'good@bad@uk.in', 'nasa@usa12.spac
 
 pattern_for_emil_verification = "^[a-zA-Z].[a-zA-Z0-9_-]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,5}$"
 
-verified_emails = []
-for email in emails:
-    if re.match(pattern_for_emil_verification,email):
-        verified_emails.append(email)
+verified_emails = [ email for email in emails if re.match(pattern_for_emil_verification,email) ]
 print(verified_emails)
